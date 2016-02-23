c44c8a83ed3a62da7fde271ac002803754fc32a1
TheKnerd <kclayengineer@cableone.net>
2016-02-23 11:32:00 -0800

    Custom datatypes
    imported and using numpy for ints and unsigned ints.
    
    still working on the floating point implementation.
    
    [ticket: X]
    
    Changes to be committed:
    	modified:   bentonMachine.py
    	modified:   numbers.py

--- numbers.py
+++ numbers.py
@@ -1,129 +1,183 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
-class Numbers:
-	'''Just a wrapper class for my datatypes.'''
+import numpy as np
 
-	class float8:
-		'''Defines numbers in 8-bit standard notation.'''
-		def __init__(self, bitString=''):
-			self.bitString = bitString
-			self.sign = self.bitString[0]
-			self.exponent = self.bitString[1:4]
-			self.mentassa = self.bitString[5:]
+class Float8:
+	'''Defines numbers in 8-bit standard notation.'''
+	0001111
+	def __init__(self, value):
+		self.setValue(self, value)
 
-		def getValue(self, type='num'):
-			if type == 'num':
-				return self.value
-			elif type == 'bin':
-				return 'b'+self.bitString
-			elif type == 'hex':
-				return hex(int('b'+self.bitString))
+	def getValue(self, type='num'):
+		if type == 'num':
+			return self.value
+		elif type == 'bin':
+			return '0b'+self.sign+self.exponent+self.mantissa
+		elif type == 'hex':
+			return hex(self.value)
+		else:
+			return 'Unknown type'
+
+	def setValue(self, value):
+		if 'float' in type(value):
+			# convert float to int
+			fits = False
+			value = str(value).split('.')
+			value[0], value[1] = int(value[0), int(value[1])
+			# value is two ints, around the decimal
+			# check to make sure that the mantissa isn't too large
+			if value[0]+value[1] > 31:
+				# too bad
+				pass
 			else:
-				return 'Unknown type'
-
-		def setValue(self, value):
-			if 'float' in type(value):
-				# some logic to change into useful datatype
+				# check to make sure that the exponent isn't too large
+				# float the shit out of value[0] and concat with value[1]
+				# find out how far the radix point moved
+				# if moved is greater than 3, too bad
 				pass
-			elif 'int' in type(value):
-				# some logic to change into float
-				pass
-			elif 'str' in type(value):
-				# some logic to change bin to float
-				# some logic to change hex to float
-				pass
+		if 'str' in type(value):
+			# some logic to change hex to float
+			if value[1] == 'x':
+				temp = 0
+				count = 0
+				value = value[2:]
+				for i in range(len(value),1,-1):
+					temp += value[count]*(16**(i-1))
+					count+=1
+				value = temp
+			# some logic to change bin to float
+			elif value[] == 'b':
+				temp = 0
+				count = 0
+				value = value[2:]
+				for i in ranche(len(value),1,-1):
+					temp += value[count]*(2**(i-1))
+					count+=1
+				value = temp
+		self.value = value
+		self.sign = bin(self.value)[2]
+		self.exponent = bin(self.value)[3:-4]
+		self.mantissa = bin(self.value)[-4:]
 
-		def getNum(self):
-			return self.getValue(type='num')
+	def getNum(self):
+		return self.getValue(type='num')
 
-		def getHex(self):
-			return self.getValue(type='hex')
+	def getHex(self):
+		return self.getValue(type='hex')
 
-		def getBin(self):
-			return self.getValue(type='bin')
+	def getBin(self):
+		return self.getValue(type='bin')
 
-		def isPositive(self):
-			if self.sign == '1':
-				return True
-			elif self.sign == '0':
-				return False
+	def isPositive(self):
+		if self.sign == '1':
+			return True
+		elif self.sign == '0':
+			return False
 
-		def getMentassa(self):
-			return 'b'+self.mentassa
+	def getMentassa(self):
+		return 'b'+self.mentassa
 
-		def getSign(self):
-			return 'b'+self.sign
+	def getSign(self):
+		return 'b'+self.sign
 
-		def getExponent(self):
-			return 'b'+self.exponent
+	def getExponent(self):
+		return 'b'+self.exponent
 
-	class sign8:
-		'''Defines numbers in two's compliment.'''
-		def __init__(self, bitString=''):
-			self.bitString = bitString
-			self.sign = self.bitString[0]
-			self.bitValue = self.bitString[1:]
-			self.value = int('b'+self.bitValue) * (-1)
+class Sign8:
+	'''Defines numbers in two's compliment. Uses numpy int8.'''
+	def __init__(self, value):
+		self.setValue(value)
 
-		def setValue(self, value):
-			if 'int' in type(value):
-				# run some logic to convert the given value into 2's compilment
-				pass
+	def setValue(self, value):
+		if 'str' in type(value):
+			isPos = 1
+			if value[0] == '-':
+				isPos = -1
+				value = value[1:]
+			if value[1] == 'x':
+				temp = 0
+				count = 0
+				value = value[2:]
+				for i in range(len(value),1,-1):
+					temp += value[count]*(16**(i-1))
+					count+=1
+				value = temp
+			elif value[] == 'b':
+				temp = 0
+				count = 0
+				value = value[2:]
+				for i in ranche(len(value),1,-1):
+					temp += value[count]*(2**(i-1))
+					count+=1
+				value = temp
+			value = value*isPos
+		self.value = np.int8(value)
 
-		def getValue(self, type='num'):
-			if type == 'num':
-				return self.value
-			elif type == 'hex':
-				return hex(int('b'+self.bitString))
-			elif type == 'bin':
-				return self.bitString
-			else:
-				return 'Unknown type'
+	def getValue(self, type='num'):
+		if type == 'num':
+			return self.value
+		elif type == 'hex':
+			return hex(int('b'+self.bitString))
+		elif type == 'bin':
+			return self.bitString
+		else:
+			return 'Unknown type'
 
-		def getNum(self):
-			return getValue(type='num')
+	def getNum(self):
+		return getValue(type='num')
 
-		def getHex(self):
-			return getValue(type='hex')
+	def getHex(self):
+		return getValue(type='hex')
 
-		def getBin(self):
-			return getValue(type='bin')
+	def getBin(self):
+		return getValue(type='bin')
 
-		def isPositive(self):
-			if self.sign == '1':
-				return True
-			else:
-				return False
+	def isPositive(self):
+		if self.sign == '0':
+			return True
+		else:
+			return False
 
-	class unsign8:
-		'''Just an unsigned integer.'''
-		def __init__(self, bitString=''):
-			self.bitString = bitString
-			self.value = self.bitString
+class UnSign8:
+	'''Just an unsigned integer. Uses numpy uint8.'''
+	def __init__(self, value):
+		self.setValue(value)
 
-		def setValue(self, value):
-			if 'int' in type(value):
-				# some logic to keep the value
-			elif 'str' in type(value):
-				# some logic to convert bin to int
-				# some logic to convert hex to int
-				pass
+	def setValue(self, value):
+		if 'str' in type(value):
+			if value[1] == 'x':
+				temp = 0
+				count = 0
+				value = value[2:]
+				for i in range(len(value),1,-1):
+					temp += value[count]*(16**(i-1))
+					count+=1
+				value = temp
+			elif value[] == 'b':
+				temp = 0
+				count = 0
+				value = value[2:]
+				for i in ranche(len(value),1,-1):
+					temp += value[count]*(2**(i-1))
+					count+=1
+				value = temp
+		self.value = np.uint8(value)
 
-		def getValue(self, type='num'):
-			if type == 'num':
-				return int('b'+self.bitString)
-			elif type == 'bin':
-				return 'b'+self.bitString
-			elif type == 'hex':
-				return hex(int('b'+self.bitString))
-			else:
-				return 'Unknown type'
+	def getValue(self, type='num'):
+		if type == 'num':
+			return self.value
+		elif type == 'bin':
+			return bin(self.value)
+		elif type == 'hex':
+			return hex(self.value)
+		else:
+			return 'Unknown type'
 
-		def getNum(self):
-			return self.getValue(type='num')
+	def getNum(self):
+		return self.getValue(type='num')
 
-		def getHex(self):
-			return self.getValue(type='hex')
+	def getHex(self):
+		return self.getValue(type='hex')
 
-		def getBin(self):
-			return self.getValue(type='bin')
+	def getBin(self):
+		return self.getValue(type='bin')
